813f5dcdc4fd982da57ae84aaf50a03d5beed02f
glebRSK <gleb172v@gmail.com>
2023-09-03 19:12:17 +0300

    feat: Added functional for recursive walking by directory and reads metadata

--- exif_reader/src/main.rs
+++ exif_reader/src/main.rs
@@ -1,3 +1,20 @@
+mod directory_reader;
+mod message;
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("exif_reader")
+        .version(option_env!("CARGO_PKG_VERSION").unwrap_or(""))
+        .arg(
+            Arg::with_name("directory")
+                .short('d')
+                .long("directory")
+                .help("Specify directory with photo")
+                .takes_value(true)
+                .required(true)
+        )
+        .get_matches();
+
+    let directory = matches.value_of("directory").unwrap();
+    directory_reader::walking(directory);
 }
