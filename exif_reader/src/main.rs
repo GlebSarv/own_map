9a2aa61a4ed318a27b48db3c4e3a43c12e40385c
glebRSK <gleb172v@gmail.com>
2023-09-06 14:50:36 +0300

    feat: Added grpc support for execute methods

--- exif_reader/src/main.rs
+++ exif_reader/src/main.rs
@@ -4,18 +4,54 @@ mod message;
 mod producer;
 mod utils;
 
+use producer::produce;
+
 use clap::{App, Arg};
 use dotenv::dotenv;
 use log::{error, info};
-use producer::produce;
+use tonic::{transport::Server, Request, Response, Status};
+
+
+use exif_readers::exif_readers_server::{ExifReaders, ExifReadersServer};
+use exif_readers::{ExifReaderRequest, ExifReadersReply};
+
+pub mod exif_readers {
+    tonic::include_proto!("exif_readers");
+}
+
+#[derive(Debug, Default)]
+pub struct ExifReaderService{}
+
+#[tonic::async_trait]
+impl ExifReaders for ExifReaderService {
+    async fn walking_directory(
+        &self,
+        request: Request<ExifReaderRequest>
+    ) -> Result<tonic::Response<ExifReadersReply>, tonic::Status> {
+        println!("Starting request");
+        let directory_name = &request.into_inner().directory_name;
+
+        if let Ok(messages) = directory_reader::walking(directory_name) {
+            let result = produce(messages).await;
+
+            if result.is_err() {
+                error!("Error while producing messages");
+            } else {
+                info!("Succsessfully delivering messages")
+            }
+        }
+
+        Ok(Response::new(ExifReadersReply {}))
+    }
+}
 
 /// Main function
 ///
 /// Required argument `directory` to define the directory in which photos will be searched
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
-
+    
     let matches = App::new("exif_reader")
         .version(option_env!("CARGO_PKG_VERSION").unwrap_or(""))
         .arg(
@@ -28,17 +64,26 @@ async fn main() {
         )
         .get_matches();
 
-    let directory = matches.value_of("directory").unwrap();
+    // let directory = matches.value_of("directory").unwrap();
 
-    if let Ok(messages) = directory_reader::walking(directory) {
-        let result = produce(messages).await;
+    // if let Ok(messages) = directory_reader::walking(directory) {
+    //     let result = produce(messages).await;
 
-        if result.is_err() {
-            error!("Error while producing messages");
-        } else {
-            info!("Succsessfully delivering messages")
-        }
-    }
+    //     if result.is_err() {
+    //         error!("Error while producing messages");
+    //     } else {
+    //         info!("Succsessfully delivering messages")
+    //     }
+    // }
+    let addr = "127.0.0.1:50051".parse().unwrap();
+    let serv = ExifReaderService::default();
+
+    Server::builder()
+        .add_service(ExifReadersServer::new(serv))
+        .serve(addr)
+        .await?;
+
+    Ok(())    
 }
 
 #[cfg(test)]
